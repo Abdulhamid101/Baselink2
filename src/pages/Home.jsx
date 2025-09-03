acb6a6ffdd098b47d89df321502c38185d13f6b8
kafidipe Abdulhamid <kafidipeabdulhamid@gmail.com>
2025-09-03 23:59:42 +0100

    additional changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import Hero from "../component/Hero/Hero";
+import WhyBaselinks from "../component/WhyBaselinks/WhyBaselinks";
 import Explore from "../component/Explore/Explore";
+import Ready from "../component/Ready/Ready"
 import Learners from "../component/Learners/Learners";
 
 export default function Home(){
   return (
     <>
       <Hero />
+      <WhyBaselinks />
       <Explore />
       <Learners />
+      <Ready />
     </>
   );
 }
